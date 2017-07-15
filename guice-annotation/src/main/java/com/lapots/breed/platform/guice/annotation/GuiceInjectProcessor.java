5e18746ed26e53f9d88bd4695f5d7504271cf482
Lapots <sebastrident@gmail.com>
2017-07-15 15:19:58 +0300

    Complete class generation added

--- guice-annotation/src/main/java/com/lapots/breed/platform/guice/annotation/GuiceInjectProcessor.java
+++ guice-annotation/src/main/java/com/lapots/breed/platform/guice/annotation/GuiceInjectProcessor.java
@@ -1,8 +1,11 @@
 package com.lapots.breed.platform.guice.annotation;
 
 import com.google.inject.AbstractModule;
-import com.squareup.javapoet.JavaFile;
-import com.squareup.javapoet.TypeSpec;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.squareup.javapoet.*;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
@@ -10,10 +13,14 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.MirroredTypeException;
+import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import javax.tools.FileObject;
 import javax.tools.StandardLocation;
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
 import java.util.Set;
 
 @SupportedAnnotationTypes("com.lapots.breed.platform.guice.annotation.GuiceInject")
@@ -32,7 +39,7 @@ public class GuiceInjectProcessor extends AbstractProcessor {
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         boolean hadElement = false;
         for (Element element : roundEnv.getElementsAnnotatedWith(GuiceInject.class)) {
-            generateGuiceInjectorClass();
+            generateGuiceInjectorClass(); // generate Guice infrastructure
 
             GuiceInject annotation = element.getAnnotation(GuiceInject.class);
 
@@ -53,42 +60,58 @@ public class GuiceInjectProcessor extends AbstractProcessor {
         return hadElement;
     }
 
-    /*
-        Generates source code for class
-
-        package com.lapots.breed.platform.guice.generator;
-
-        import com.google.inject.Guice;
-        import com.google.inject.Injector;
-        import com.google.inject.Module;
-        import java.util.ArrayList;
-        import java.util.List;
-
-        public class $GuiceInjector {
-            private static Injector injector;
-            private static List<Module> modules = new ArrayList<>();
-
-            public static void addModule(Module module) { modules.add(module); }
-
-            public static void init() { injector = Guice.createInjector(modules); }
-
-            public static <T> T getInstance(Class<T> clazz) {
-                return injector.getInstance(clazz);
-            }
-        }
-     */
-
     private void generateGuiceInjectorClass() {
         try {
             // check file existence
             filer.getResource(StandardLocation.SOURCE_PATH,
                     "com.lapots.breed.platform.guice.generated", "$GuiceInjector");
         } catch (IOException e) {
-            // e.printStackTrace(); // file not found
-
+            // public class $GuiceInjector
             TypeSpec.Builder guiceInjectorClass = TypeSpec
                     .classBuilder("$GuiceInjector")
                     .addModifiers(Modifier.PUBLIC);
+
+            // private static Injector injector
+            FieldSpec injectorField = FieldSpec.builder(Injector.class, "injector")
+                    .addModifiers(Modifier.PRIVATE, Modifier.STATIC)
+                    .build();
+            guiceInjectorClass.addField(injectorField);
+
+            // private static List<Module> modules = new ArrayList<>();
+            FieldSpec modulesField = FieldSpec.builder(ParameterizedTypeName.get(List.class, Module.class), "modules")
+                    .addModifiers(Modifier.PRIVATE, Modifier.STATIC)
+                    .initializer("new $T<>()", ClassName.get("java.util", "ArrayList"))
+                    .build();
+            guiceInjectorClass.addField(modulesField);
+
+            // public static void addModule(Module module) { modules.add(module; }
+            MethodSpec addModuleMethod = MethodSpec.methodBuilder("addModule")
+                    .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                    .returns(void.class)
+                    .addParameter(Module.class, "module")
+                    .addStatement("modules.add($N)", "module")
+                    .build();
+            guiceInjectorClass.addMethod(addModuleMethod);
+
+            // public static void init() { injector = Guice.createInjector(modules); }
+            MethodSpec initMethod = MethodSpec.methodBuilder("init")
+                    .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                    .returns(void.class)
+                    .addStatement("$N = $T.createInjector($N)", "injector",
+                            ClassName.get("com.google.inject", "Guice"), "modules")
+                    .build();
+            guiceInjectorClass.addMethod(initMethod);
+
+            // public static <T> T getInstance(Class<T> clazz) { return injector.getInstance(clazz); }
+            TypeVariableName typeVariableName = TypeVariableName.get("T");
+            MethodSpec getInstanceMethod = MethodSpec.methodBuilder("getInstance")
+                    .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                    .addTypeVariable(typeVariableName)
+                    .addParameter(ParameterizedTypeName.get(ClassName.get(Class.class), typeVariableName), "clazz")
+                    .addStatement("return $N.getInstance($N)", "injector", "clazz")
+                    .returns(typeVariableName)
+                    .build();
+            guiceInjectorClass.addMethod(getInstanceMethod);
 
             try {
                 JavaFile.builder("com.lapots.breed.platform.guice.generated", guiceInjectorClass.build())
